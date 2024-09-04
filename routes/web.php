3f5cb155444a13798c107344d82713426c818c06
arata-Engineer <arataspace1@gmail.com>
2024-09-05 06:20:18 +0900

    commit_kadai_laravel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 
 Route::get('/posts', [PostController::class, 'index']);
